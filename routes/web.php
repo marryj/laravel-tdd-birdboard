8869c3853cca9bf74ae6890cb9d76f8f0515958c
Marry <eng.marry@gmail.com>
2024-05-30 11:35:45 +0300

    Projects Test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post("projects", 'App\Http\Controllers\ProjectsController@store');
+
+Route::get("projects", 'App\Http\Controllers\ProjectsController@index')->name('projects.index');
---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_a_user_can_create_a_project(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            "title" => $this->faker->title,
+            "description" => $this->faker->paragraph,
+
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('projects.index');
+
+        $this->assertDatabaseHas("projects", $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+}
